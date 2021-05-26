cfa8a1fe7ddca16941f9fbf6aded513de60f046b
guan <whattamings@163.com>
2021-05-26 22:41:53 +0800

    Initial commit

---
+++ src/main/java/com/kare/pojo/Book.java
@@ -0,0 +1,73 @@
+package com.kare.pojo;
+
+public class Book {
+    private Integer id;
+
+    private String bookname;
+
+    private String author;
+
+    private Double price;
+
+    private Integer classid;
+
+    private String descr;
+
+    private String path;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname == null ? null : bookname.trim();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author == null ? null : author.trim();
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getClassid() {
+        return classid;
+    }
+
+    public void setClassid(Integer classid) {
+        this.classid = classid;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr == null ? null : descr.trim();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+}
